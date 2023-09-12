27626d59ba81d44cdd7155d714a890c48004418d
JC949 <jacobcampbell180@yahoo.com>
2023-09-12 13:51:28 -0700

    Added a function to display copyright

---
+++ src/functions/Copyright.js
@@ -0,0 +1,12 @@
+function Copyright() {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <p>&copy; {new Date().getFullYear()} Code Cove. All rights reserved.</p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Copyright;
+  
