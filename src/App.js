42d05f102caa5f2dd3ae60943bad7e70c62df2d8
JC949 <jacobcampbell180@yahoo.com>
2023-10-31 02:01:02 -0700

    Add StudentComponent to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import NavBar from "./components/NavBar";
 import CoursesPage from "./pages/CoursesPage";
 import LandingPage from "./pages/LandingPage";
 import AuthenticationPage from "./pages/AuthenticationPage";
 import CourseDetailsPage from "./pages/CourseDetailsPage";
 import { ClerkProvider } from "@clerk/clerk-react";
+import StudentComponent from "./components/StudentComponent";
 
-
-// App function returns NavBar, BrowserRouter and ClerkProviderWithRoutes functions
+/* Define pub key in variable, make sure clerk provider is called in App.js
+so we can use clerk features throughout app without needing to re call */
 function App() {
-
   if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
     throw new Error("Missing Publishable Key");
   }
@@ -18,20 +17,16 @@ function App() {
   const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
   return (
-    <>
     <ClerkProvider publishableKey={clerkPubKey}>
-
-    <NavBar />
-    <Routes>
-      <Route path ="/" element = {<LandingPage />} />
-      <Route path ="/courses" element = {<CoursesPage />} />
-      <Route path="/courses/:courseId" element={<CourseDetailsPage />} />
-      <Route path="/login" element = {<AuthenticationPage />} />
-    </Routes>
-
-      </ClerkProvider>
-    </>
+      <StudentComponent />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/courses" element={<CoursesPage />} />
+        <Route path="/courses/:courseId" element={<CourseDetailsPage />} />
+        <Route path="/login" element={<AuthenticationPage />} />
+      </Routes>
+    </ClerkProvider>
   );
 }
 
-export default App
+export default App;
